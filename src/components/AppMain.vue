dc1382377f1dec322b87b147d54f75261d121c07
giordanofabrizii <giordanofabrizi@gmail.com>
2024-08-21 15:32:41 +0200

    added single component project

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,15 +1,43 @@
 <script>
+import axios from 'axios';
+import ProjectCard from './ProjectCard.vue';
+
 export default{
     data() {
         return{
-            
+            projects : [],
         }
+    },
+    components: {
+        ProjectCard,
+    },
+    methods: {
+        getProjects() {
+            axios.get('http://127.0.0.1:8000/api/projects/', {
+                params: {
+                    nome: '',
+                }
+            })
+                .then((response) => {
+                    console.log(response.data.results.data)
+                    this.projects = response.data.results.data;
+                })
+                .catch(function (error) {
+                    console.log(error);
+                })
+                .finally(function () {
+                    // always executed
+                });
+        },
+    },
+    mounted(){
+        this.getProjects();
     }
 }
 </script>
 
 <template>
-    <h1>CIAOOO</h1>
+    <ProjectCard v-for="project in projects" :project="project"/>
 </template>
 
 <style scoped>
---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,42 @@
+<script>
+export default{
+    data() {
+        return{
+            
+        }
+    },
+    props: [
+        'project'
+    ],
+}
+</script>
+
+<template>
+    <h1>{{ project.title }}</h1>
+    <img :src="project.image" alt="">
+    <p :style="{ backgroundColor: project.type.color }">{{ project.type.name }}</p>
+    <ul>
+        <li v-for="tech in project.technologies">
+            {{ tech.name }}
+        </li>
+    </ul>
+</template>
+
+<style lang="scss">
+p{
+    display: flex;
+    width: fit-content;
+    padding: .5rem;
+    border-radius: 8px;
+}
+
+ul {
+    display: flex;
+    flex-direction: row;
+    li{
+        list-style-type: none;
+        margin-right: .5rem;
+    }
+}
+
+</style>
